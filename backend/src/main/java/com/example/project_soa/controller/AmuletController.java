22c7aa5f8ec09f520437223a4a7a7b923f470329
Aom0114Wasina <oomkoopo@gmail.com>
2025-03-09 19:04:58 +0700

    add getall,getid,delete,post,searchallkeyword,searchbypricerange

--- backend/src/main/java/com/example/project_soa/controller/AmuletController.java
+++ backend/src/main/java/com/example/project_soa/controller/AmuletController.java
@@ -16,19 +16,40 @@ public class AmuletController {
     @Autowired
     private AmuletService amuletService;
 
-    @GetMapping("/")
-    public ResponseEntity<List<Amulet>> getAllAmulets() {
-        return ResponseEntity.ok(amuletService.getAllAmulets());
+    @GetMapping
+    public ResponseEntity<List<Amulet>> getAll() {
+        return ResponseEntity.ok(amuletService.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Amulet> getAmuletById(@PathVariable Integer id) {
-        return ResponseEntity.ok(amuletService.getAmuletById(id));
+    public ResponseEntity<Amulet> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(amuletService.getById(id));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteAmulet(@PathVariable Integer id) {
-        amuletService.deleteAmulet(id);
+    public ResponseEntity<String> delete(@PathVariable Integer id) {
+        amuletService.delete(id);
         return ResponseEntity.ok("Amulet has been removed.");
     }
+
+    @PostMapping
+    public ResponseEntity<Amulet> create(@RequestBody Amulet amulet) {
+        return ResponseEntity.ok(amuletService.create(amulet));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Amulet> update(@PathVariable Integer id, @RequestBody Amulet updatedAmulet) {
+        return ResponseEntity.ok(amuletService.update(id, updatedAmulet));
+    }
+    @GetMapping("/search")
+    public ResponseEntity<List<Amulet>> search(@RequestParam String keyword) {
+        return ResponseEntity.ok(amuletService.search(keyword));
+    }
+
+    @GetMapping("/price-range")
+    public ResponseEntity<List<Amulet>> searchByPriceRange(@RequestParam int minPrice,@RequestParam int maxPrice) 
+    {
+        return ResponseEntity.ok(amuletService.searchByPriceRange(minPrice, maxPrice));
+    }
+
 }
--- backend/src/main/java/com/example/project_soa/service/AmuletService.java
+++ backend/src/main/java/com/example/project_soa/service/AmuletService.java
@@ -1,32 +1,59 @@
 package com.example.project_soa.service;
 
-import com.example.project_soa.model.Amulet;
-import com.example.project_soa.repository.AmuletRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.example.project_soa.model.Amulet;
+import com.example.project_soa.repository.AmuletRepository;
 
 @Service
 public class AmuletService {
-
+    
     @Autowired
     private AmuletRepository amuletRepository;
 
-    public List<Amulet> getAllAmulets() {
+    public List<Amulet> getAll() {
         return amuletRepository.findAll();
     }
 
-    public Amulet getAmuletById(Integer id) {
-        Optional<Amulet> amulet = amuletRepository.findById(id);
-        return amulet.orElseThrow(() -> new RuntimeException("Amulet not found with ID: " + id));
+    public Amulet getById(int id) {
+        return amuletRepository.findById(id).orElseThrow(() -> 
+            new RuntimeException("Amulet not found with ID: " + id));
     }
 
-    public void deleteAmulet(Integer id) {
-        if (!amuletRepository.existsById(id)) {
-            throw new RuntimeException("Amulet not found with ID: " + id);
-        }
+    public Amulet update(int id, Amulet updatedAmulet) {
+        Amulet existingAmulet = this.getById(id);
+        
+        if (updatedAmulet.getName() != null && !updatedAmulet.getName().isEmpty())
+            existingAmulet.setName(updatedAmulet.getName());
+        
+        if (updatedAmulet.getTempleName() != null && !updatedAmulet.getTempleName().isEmpty())
+            existingAmulet.setTempleName(updatedAmulet.getTempleName());
+        
+        if (updatedAmulet.getPrice() > 0)
+            existingAmulet.setPrice(updatedAmulet.getPrice());
+
+        if (updatedAmulet.getType() != null && !updatedAmulet.getType().isEmpty())
+            existingAmulet.setType(updatedAmulet.getType());
+        
+        return amuletRepository.save(existingAmulet);
+    }
+
+    public Amulet create(Amulet amulet) {
+        return amuletRepository.save(amulet);
+    }
+
+    public void delete(int id) {
         amuletRepository.deleteById(id);
+    }
+    
+    public List<Amulet> search(String keyword) {
+        return amuletRepository.findByKeyWord(keyword);
+    }
+    
+    public List<Amulet> searchByPriceRange(int minPrice,int maxPrice){
+    	return amuletRepository.findByPriceBetween(minPrice, maxPrice);
     }
 }
